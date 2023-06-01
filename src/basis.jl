d27ec91b17e2e53d364e2254b5406849ee94126d
lukazmm <lukas.m.mesicek@gmail.com>
2023-06-01 16:51:43 -0600

    Work on basis operators

--- src/basis.jl
+++ src/basis.jl
@@ -9,8 +9,23 @@ export Monomials, tensor_basis
 ## Core Types ##########
 ########################
 
+"""
+Represents an analytic function, linear combinations of which approximate numerical functions across nodes.
+"""
 abstract type BasisFunction end
 
+(basis::BasisFunction)(x) = error("Application of Basis Function of type $(typeof(basis)) is undefined")
+
+"""
+Represents an analytic operator which may be applied to a basis function
+"""
+abstract type BasisOperator end
+
+(basis::BasisOperator)(_::BasisFunction) = error("Application of Basis Operator of type $(typeof(basis)) is undefined")
+
+"""
+Represents a set of basis functions. 
+"""
 struct BasisFunctions{B <: BasisFunction}
     bases::Vector{B}
 end
@@ -18,9 +33,18 @@ end
 Base.length(basis::BasisFunctions) = length(basis.bases)
 Base.getindex(basis::BasisFunctions, i) = getindex(basis.bases, i)
 
-abstract type Derivative end
+########################
+## Derivative ##########
+########################
 
-derivative(func::BasisFunction)::BasisFunction = error("Analytic derivative not implemented for $(typeof(func))")
+struct Partial{S, N} <: BasisOperator 
+    component::Int
+
+    Partial{S, N}(component::Int) = component ≤ S ? new{S, N}(component) : error("Component of partial $(component) must be ≤ $(S)")
+end
+
+struct Gradient{S} <: BasisOperator end
+
 
 ########################
 ## Monomial ############
@@ -33,8 +57,6 @@ For instance x z^2 is represented as [1, 0, 2].
 """
 struct Monomial{S} <: BasisFunction
     powers::SVector{S, UInt}
-
-    Monomial{S}(x) where {S} = new{S}(SVector{S, UInt}(x))
 end
 
 function (monomial::Monomial)(x::AbstractVector{F})::F where {F}
@@ -45,6 +67,22 @@ function (monomial::Monomial)(x::AbstractVector{F})::F where {F}
     result
 end
 
+function (partial::Partial{S, N})(monomial::Monomial{S}) where {S, N}
+    power = @inbounds monomial.powers[partial.component]
+
+    if power < N
+        Monomial(@SVector zeros(S))
+    else
+        coefficient = one(UInt)
+
+        for i in (power-N):power
+            coefficient *= i
+        end
+
+        Monomial(@SVector zeros(S))
+    end
+end
+
 """
     tensor_basis(S, order)
 
@@ -52,6 +90,5 @@ Constructs a set of basis vectors for an `S` dimensional space, covering all per
 """
 function tensor_basis(S, order)
     orders = Iterators.product([1:order for _ in 1:S]...)
-    BasisFunctions(vec(collect(map(Monomial{S}, orders))))
+    BasisFunctions(vec(collect(map((x) -> Monomial{S}(SVector{S, UInt}(x)), orders))))
 end
-
