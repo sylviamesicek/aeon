e94ac1ce4a1dd3a9bc5e035f66cbde9bf8878bb3
Ben Derieg <bderieg@deriegfamily.com>
2025-08-21 16:34:47 -0600

    fixed refinement now fixes more slowly to avoid interpolation errors

--- tools/asphere/src/run/evolve.rs
+++ tools/asphere/src/run/evolve.rs
@@ -192,102 +192,99 @@ pub fn evolve_data_full(
         let h = mesh.min_spacing() * config.evolve.cfl;
 
         // Perform regridding to a fixed value if configured as such, and if at a late enough time
-        if config.regrid.fix_grid && proper_time >= config.regrid.fix_grid_time && !fixed_grid {
-            fixed_grid = true;
+        if steps_since_regrid > config.regrid.flag_interval && config.regrid.fix_grid && proper_time >= config.regrid.fix_grid_time && !fixed_grid {
+            steps_since_regrid = 0;
 
-            // Loop through regridding steps until we reach the desired level
-            loop {
-                // Check if we are at the desired level
-                let mut min_level = usize::MAX;
-                let mut max_level = usize::MIN;
-                for cell in mesh.tree().active_cell_indices() {
-                    let ll = mesh.tree().active_level(cell);
-                    let cc = mesh.tree().active_bounds(cell).center()[0]; // get 1st element because we only have 1 dimension
-                    if cc < config.regrid.fix_grid_radius {
-                        if ll < min_level {
-                            min_level = ll;
-                        }
-                        if ll > max_level {
-                            max_level = ll;
-                        }
+            // Check if we are at the desired level
+            let mut min_level = usize::MAX;
+            let mut max_level = usize::MIN;
+            for cell in mesh.tree().active_cell_indices() {
+                let ll = mesh.tree().active_level(cell);
+                let cc = mesh.tree().active_bounds(cell).center()[0]; // get 1st element because we only have 1 dimension
+                if cc < config.regrid.fix_grid_radius {
+                    if ll < min_level {
+                        min_level = ll;
+                    }
+                    if ll > max_level {
+                        max_level = ll;
                     }
                 }
-                if min_level == config.regrid.fix_grid_level
-                    && max_level == config.regrid.fix_grid_level
-                {
+            }
+            if min_level == config.regrid.fix_grid_level
+                && max_level == config.regrid.fix_grid_level
+            {
+                fixed_grid = true;
+            }
+            // // If radius is smaller than the smallest cell, what do we do?
+            let mut refine_inner = false;
+            let mut coarsen_inner = false;
+            if min_level == usize::MAX || max_level == usize::MIN {
+                // If too refined, then coarsen the innermost
+                if mesh.num_levels() > config.regrid.fix_grid_level {
+                    coarsen_inner = true;
+                // If too coarse, then refine the innermost
+                } else if mesh.num_levels() < config.regrid.fix_grid_level {
+                    refine_inner = true;
+                // If at max refinement, then just stop
+                } else {
                     break;
                 }
-                // // If radius is smaller than the smallest cell, what do we do?
-                let mut refine_inner = false;
-                let mut coarsen_inner = false;
-                if min_level == usize::MAX || max_level == usize::MIN {
-                    // If too refined, then coarsen the innermost
-                    if mesh.num_levels() > config.regrid.fix_grid_level {
-                        coarsen_inner = true;
-                    // If too coarse, then refine the innermost
-                    } else if mesh.num_levels() < config.regrid.fix_grid_level {
-                        refine_inner = true;
-                    // If at max refinement, then just stop
-                    } else {
-                        break;
-                    }
-                }
-
-                // Perform constraint assessment
-                if buffers_filled.iter().all(|&f| f) {
-                    deriv_buffer.resize(mesh.num_nodes(), 0.0);
-                    deriv_buffer.fill(0.0);
-
-                    const WEIGHTS: [f64; 5] = [-1. / 12., 2. / 3., 0.0, -2. / 3., 1. / 12.];
-
-                    for si in 0..5 {
-                        let part = deriv_buffers[(buffer_index - si) % 5].as_slice();
-
-                        assert!(part.len() == deriv_buffer.len());
-
-                        for i in 0..deriv_buffer.len() {
-                            deriv_buffer[i] += WEIGHTS[si] * part[i]
-                        }
-                    }
-
-                    for i in 0..deriv_buffer.len() {
-                        deriv_buffer[i] /= h;
-                    }
-
-                    let constraint_buffer = constraint_buffers[(buffer_index - 2) % 5].as_slice();
-                    assert!(deriv_buffer.len() == constraint_buffer.len());
-
-                    for i in 0..deriv_buffer.len() {
-                        deriv_buffer[i] -= constraint_buffer[i];
-                    }
-
-                    constraint = mesh.l2_norm(&deriv_buffer);
-                }
-
-                // Take one regridding step towards desired level
-                if refine_inner {
-                    mesh.refine_innermost();
-                } else if coarsen_inner {
-                    mesh.coarsen_innermost();
-                } else {
-                    mesh.regrid_in_radius(
-                        config.regrid.fix_grid_radius,
-                        config.regrid.fix_grid_level,
-                    );
-                }
-
-                // Copy system into tmp scratch space (provided by dissipation).
-                let scratch = integrator.scratch(system.contigious().len());
-                scratch.copy_from_slice(system.contigious());
-                system.resize(mesh.num_nodes());
-                mesh.transfer_system(
-                    Order::<4>,
-                    SystemSlice::from_contiguous(&scratch, &Fields),
-                    system.as_mut_slice(),
-                );
-
-                buffers_filled.fill(false);
             }
+
+            // Perform constraint assessment
+            if buffers_filled.iter().all(|&f| f) {
+                deriv_buffer.resize(mesh.num_nodes(), 0.0);
+                deriv_buffer.fill(0.0);
+
+                const WEIGHTS: [f64; 5] = [-1. / 12., 2. / 3., 0.0, -2. / 3., 1. / 12.];
+
+                for si in 0..5 {
+                    let part = deriv_buffers[(buffer_index - si) % 5].as_slice();
+
+                    assert!(part.len() == deriv_buffer.len());
+
+                    for i in 0..deriv_buffer.len() {
+                        deriv_buffer[i] += WEIGHTS[si] * part[i]
+                    }
+                }
+
+                for i in 0..deriv_buffer.len() {
+                    deriv_buffer[i] /= h;
+                }
+
+                let constraint_buffer = constraint_buffers[(buffer_index - 2) % 5].as_slice();
+                assert!(deriv_buffer.len() == constraint_buffer.len());
+
+                for i in 0..deriv_buffer.len() {
+                    deriv_buffer[i] -= constraint_buffer[i];
+                }
+
+                constraint = mesh.l2_norm(&deriv_buffer);
+            }
+
+            // Take one regridding step towards desired level
+            if refine_inner {
+                mesh.refine_innermost();
+            } else if coarsen_inner {
+                mesh.coarsen_innermost();
+            } else {
+                mesh.regrid_in_radius(
+                    config.regrid.fix_grid_radius,
+                    config.regrid.fix_grid_level,
+                );
+            }
+
+            // Copy system into tmp scratch space (provided by dissipation).
+            let scratch = integrator.scratch(system.contigious().len());
+            scratch.copy_from_slice(system.contigious());
+            system.resize(mesh.num_nodes());
+            mesh.transfer_system(
+                Order::<4>,
+                SystemSlice::from_contiguous(&scratch, &Fields),
+                system.as_mut_slice(),
+            );
+
+            buffers_filled.fill(false);
 
             continue;
         }
